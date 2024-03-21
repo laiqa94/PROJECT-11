72af5f014fc3147a7dbd8de45ef32cee3a25b8e7
laiqa khan <152205933+laiqa94@users.noreply.github.com>
2024-03-22 01:47:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['laiqa', 'maryam', 'zainab', 'sana'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ main.ts
@@ -0,0 +1,9 @@
+
+
+let names :string[]= ['laiqa','maryam','zainab','sana'];
+
+for(let i=0; i<names.length; i++){
+
+console.log(names[i]) ;
+
+}
